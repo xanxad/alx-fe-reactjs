615ee0823937d3b9c3bad978e291f89178551c29
Biniam Aregay <seifubiniam6@gmail.com>
2024-08-11 21:14:36 +0300

    add JSX components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,15 @@
+import Header from "./components/Header";
 import WelcomeMessage from "./components/WelcomeMessage";
+import MainCOntent from "./components/MainContent";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <div>
+      <Header />
       <WelcomeMessage />
+      <MainCOntent />
+      <Footer />
     </div>
   );
 };
---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainCOntent() {
+  return (
+    <main>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainCOntent;
