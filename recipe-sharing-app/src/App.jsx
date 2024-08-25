39936b4eb3a83f2c3326e313b301654d6cac9f6f
Biniam Aregay <seifubiniam6@gmail.com>
2024-08-25 20:56:27 +0300

    Implementing Advanced Search and Filtering Capabilities-2

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,17 @@
+// src/App.jsx
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 import SearchBar from "./components/SearchBar";
+import AddRecipeForm from "./components/AddRecipeForm"; // Import AddRecipeForm
 
 function App() {
   return (
     <Router>
       <div>
-        {/* SearchBar component is included in the layout */}
-        <SearchBar />
+        <SearchBar /> {/* SearchBar component included in the layout */}
+        <AddRecipeForm /> {/* AddRecipeForm component included in the layout */}
         <Routes>
           {/* Define routes for different pages */}
           <Route path="/" element={<RecipeList />} />
