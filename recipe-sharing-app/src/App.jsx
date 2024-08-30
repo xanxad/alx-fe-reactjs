55694f9115a040f710b6c2050a036db6a62d6db7
Biniam Aregay <seifubiniam6@gmail.com>
2024-08-30 13:42:58 +0300

    zustland

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,23 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
 
 const App = () => {
   return (
-    <div>
-      <RecipeList />
-      <AddRecipeForm />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <RecipeList />
+          <AddRecipeForm />
+        </Route>
+        <Route path="/recipe/:id" exact>
+          {({ match }) => (
+            <RecipeDetails recipeId={parseInt(match.params.id)} />
+          )}
+        </Route>
+      </Switch>
+    </Router>
   );
 };
-
 export default App;
---
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -0,0 +1,9 @@
+import useRecipeStore from "./recipeStore";
+
+const DeleteRecipeButton = ({ recipeId }) => {
+  const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+
+  return <button onClick={() => deleteRecipe(recipeId)}>Delete Recipe</button>;
+};
+
+export default DeleteRecipeButton;
