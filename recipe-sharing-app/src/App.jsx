dca7da2aa1dd08fa1f5c854509d786c40926cc3d
Biniam Aregay <seifubiniam6@gmail.com>
2024-08-30 14:23:08 +0300

    Initialize the Recipe Sharing Application

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,13 @@
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+
+const App = () => {
+  return (
+    <div>
+      <RecipeList />
+      <AddRecipeForm />
+    </div>
+  );
+};
+
+export default App;
