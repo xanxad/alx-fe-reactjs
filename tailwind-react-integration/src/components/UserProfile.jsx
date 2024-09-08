7386ec61322917e2aaa2838eb0bff03ce7d61058
Biniam Aregay <seifubiniam6@gmail.com>
2024-09-08 17:35:23 +0300

    Responsive Design Implementation Using Tailwind CSS-1

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,15 +1,15 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm md:max-w-md mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100  sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-md mx-auto my-20 rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 mx-auto"
+        className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
       />
       <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4">
         John Doe
       </h1>
-      <p className="text-sm sm:text-base md:text-lg text-gray-600">
+      <p className=" sm:text-sm md:text-base text-gray-600">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
