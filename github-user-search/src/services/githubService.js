52213572fef0b5564113e7daaf9dab7e2c288dc1
Biniam Aregay <seifubiniam6@gmail.com>
2024-09-27 17:49:12 +0300

    Enhancing User Interface-2

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -6,20 +6,22 @@ export const fetchUsers = async (params, page = 1) => {
   const { username, location, minRepos } = params;
   let query = username;
 
-  if (location) query += ` location:${location}`;
-  if (minRepos) query += ` repos:>=${minRepos}`;
+  if (location) query += `+location:${location}`;
+  if (minRepos) query += `+repos:>=${minRepos}`;
 
   try {
-    const response = await axios.get(GITHUB_API_BASE_URL, {
-      params: {
-        q: query,
-        per_page: 30,
-        page: page,
-      },
-      headers: {
-        Authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`,
-      },
-    });
+    const response = await axios.get(
+      `${GITHUB_API_BASE_URL}?q=${encodeURIComponent(query)}`,
+      {
+        params: {
+          per_page: 30,
+          page: page,
+        },
+        headers: {
+          Authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`,
+        },
+      }
+    );
     return response.data;
   } catch (error) {
     console.error("Error fetching users:", error);
